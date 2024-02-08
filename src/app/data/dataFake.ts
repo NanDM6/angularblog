8ef0507002bf66782d78140de5732b029eb0607b
Renan <85131972+NanDM6@users.noreply.github.com>
2024-02-07 22:20:58 -0300

    Add files via upload

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,34 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'ESTREIA DE SOLO LEVELING',
+    description:
+      'Sem sombra de dúvidas, um dos animes mais esperados da temporada. Na verdade, um dos animes mais esperados há muito tempo. Esta obra prima dos estúdios sul-coreanos iniciou seu lançamento em 2016. Apesar do manhwa ser colorido e MUITO bem detalhado e desenhado, nada como ver uma adaptação em anime, não é mesmo? Por enquanto o anime tem tido uma excelente adaptação e aceitação dos fãs.',
+    photoCover:
+      'https://conteudo.imguol.com.br/c/entretenimento/0e/2024/01/09/anime-de-solo-leveling-1704828915890_v2_900x506.png.webp',
+  },
+  {
+    id: '2',
+    title: 'Anime sobre comer monstros?! Dungeon Meshi!',
+    description:
+      'O anime Dungeon Meshi apresenta uma proposta inovadora e bem-humorada sobre a vida de aventureiros que caçam em dungeons. Muitas vezes quando animes e mangás são feitos, a questão das necessidades básicas (ir ao banheiro, tomar banho, fedor, comida, sono, etc.) não são abordadas. Esse anime traz a questão da dificuldade na alimentação em caçadas de longa duração em masmorras. A solução que o grupo encontra: comer monstros. Mas claro que não é de qualquer jeito. Eles são muito bem temperados e cozidos! Até o momento, tem sido um anime leve e divertido, recomendo.',
+    photoCover:
+      'https://tm.ibxk.com.br/2024/01/10/10102431185088.png?ims=1200x675',
+  },
+  {
+    id: '3',
+    title: 'Frieren e a Jornada para o Além',
+    description:
+      'Uma das mais gratas surpresas desta temporada. Confesso que não comecei a assistir assim que lançou, achei que fosse somente mais um anime de herói em outro mundo e overpower. No entanto, fui lendo comentários ao assistir outros animes e MUITOS falavam dele. Pensei: "Não é possível, tanta gente falando bem..." e foi nessa que comecei a assistir. E poxa, que anime delicioso. Ele é conduzido com uma leveza, uma calma, ao mesmo tempo que te prende e tem uma animação maravilhosa. Se você ainda não assistiu, pare tudo e vá AGORA mesmo.',
+    photoCover:
+      'https://ovicio.com.br/wp-content/uploads/2023/10/20231007-frieren-anime.jpg',
+  },
+  {
+    id: '4',
+    title: 'Guerra, desenvolvimento e história, Kingdom é completo',
+    description:
+      'E chegou! A 5ª temporada de KINGDOM já está lançando e mais uma vez, surpreende. A animação continua ótima, te prendendo em cada fim de episódio. Os personagens continuam sendo bem desenvolvidos e as batalhas com um leve toque realista e baseado nas histórias do passado, continuam a impressionar. Para quem já acompanhava, não perca tempo e acompanhe o lançamento todo sábado. Para quem ainda não iniciou o anime, dê uma chance (a animação inicial é meio simplória, mas a história é excelente).',
+    photoCover:
+      'https://noticiariootaku.com.br/wp-content/uploads/2023/12/5o-Temporada-de-Kingdom.jpg',
+  },
+];
